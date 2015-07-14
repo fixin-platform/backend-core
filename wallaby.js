829d8f7f4a121e50abfce01ce43fdb92368e2331
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2015-07-14 15:31:33 +0300

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -3,53 +3,63 @@ var _ = require("underscore");
 underscoreDeepExtend = require("underscore-deep-extend");
 _.mixin({deepExtend: underscoreDeepExtend(_)});
 
-var config = _.deepExtend({
-  testFramework: "mocha",
-  //files: [
-  //  "lib/**/*.coffee",
-  //  "test/ReadEcho.coffee",
-  //  "test/mocha.coffee",
-  //  "test/config.json"
-  //],
-  //tests: [
-  //  "test/**/*.coffee",
-  //  "!test/ReadEcho.coffee",
-  //  "!test/mocha.coffee"
-  //],
-  files: [
-    "**/*",
-    "!*",
-    "!.*/**/*",
-    { pattern: "bin/**/*", instrument: false, load: true, ignore: false },
-    "!node_modules/**/*",
-    "!**/*Spec.coffee"
-  ],
-  tests: [
-    "test/**/*Spec.coffee"
-  ],
-  env: {
-    type: "node",
-    runner: "node"
-  },
-  bootstrap: function (wallaby) {
-    var mocha = wallaby.testFramework;
-    mocha.ui("bdd");
-    require.main.require("test/mocha");
-    try {
-      var local = require(wallaby.localProjectDir + "/wallaby.local"); // need to require again here, because bootstrap runs in another context
-      local.bootstrap && local.bootstrap(wallaby)
-    } catch (error) {
-      if (error.code !== "MODULE_NOT_FOUND") { // unexpected!
-        throw error;
+module.exports = function(wallaby) {
+  var coffeeCompiler = wallaby.compilers.coffeeScript();
+
+  var config = _.deepExtend({
+    testFramework: "mocha",
+    //files: [
+    //  "lib/**/*.coffee",
+    //  "test/ReadEcho.coffee",
+    //  "test/mocha.coffee",
+    //  "test/config.json"
+    //],
+    //tests: [
+    //  "test/**/*.coffee",
+    //  "!test/ReadEcho.coffee",
+    //  "!test/mocha.coffee"
+    //],
+    files: [
+      "**/*",
+      "!*",
+      "!.*/**/*",
+      {pattern: "bin/**/*", instrument: false, load: true, ignore: false},
+      "!node_modules/**/*",
+      "!**/*Spec.coffee"
+    ],
+    tests: [
+      "test/**/*Spec.coffee"
+    ],
+    compilers: {
+      "lib/**/*.coffee": coffeeCompiler,
+      "test/**/*.coffee": coffeeCompiler
+    },
+    env: {
+      type: "node",
+      runner: "node"
+    },
+    bootstrap: function(wallaby) {
+      var mocha = wallaby.testFramework;
+      mocha.ui("bdd");
+      require.main.require("test/mocha");
+      try {
+        var local = require(wallaby.localProjectDir + "/wallaby.local"); // need to require again here, because bootstrap runs in another context
+        local.bootstrap && local.bootstrap(wallaby)
+      } catch (error) {
+        if (error.code !== "MODULE_NOT_FOUND") { // unexpected!
+          throw error;
+        }
       }
     }
-  }
-}, local);
+  }, local);
 
-config.env = config.env || {};
-config.env.params = config.env.params || {};
-config.env.params.env = config.env.params.env || "";
-config.env.params.env += ";ROOT_DIR="+process.cwd();
+  config.env = config.env || {};
+  config.env.params = config.env.params || {};
+  config.env.params.env = config.env.params.env || "";
+  config.env.params.env += ";ROOT_DIR=" + process.cwd();
+
+  return config;
+};
 
 /* Duplicate code, because wallaby.js and bootstrap() run in different contexts */
 function getLocalWallaby() {
@@ -64,5 +74,3 @@ function getLocalWallaby() {
   }
   return local;
 }
-
-module.exports = config;
